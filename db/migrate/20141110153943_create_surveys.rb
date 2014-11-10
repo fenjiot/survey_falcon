e03a3a92f2c0284769d419f3e737472bc88042eb
Ben Masi <ben.m.masi@gmail.com>
2014-11-10 11:49:20 -0500

    PR comments

--- db/migrate/20141110153943_create_surveys.rb
+++ db/migrate/20141110153943_create_surveys.rb
@@ -1,13 +1,13 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
-      t.string :name
-      t.text :description
-      t.integer :user_id
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :user_id, null:false
 
       t.timestamps null: false
     end
 
-    add_index :surveys, :user_id, null: false
+    add_index :surveys, :user_id
   end
 end
