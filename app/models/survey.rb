4290062b1808ccccaa1f3ff49d22500ec805e096
Ben Masi <ben.m.masi@gmail.com>
2014-11-10 11:16:04 -0500

    Users can create new surveys

---
+++ app/models/survey.rb
@@ -0,0 +1,6 @@
+class Survey < ActiveRecord::Base
+  validates :name, presence: true
+  validates :description, presence: true
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110144912) do
+ActiveRecord::Schema.define(version: 20141110153943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "surveys", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "surveys", ["user_id"], name: "index_surveys_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
