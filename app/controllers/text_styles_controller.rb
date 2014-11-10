9bf48a83ba7bf16e9a2c7f4ff542b3a5b8664d88
fenjiot <fenjiot@gmail.com>
2014-11-10 17:48:51 -0500

    Refactor text_styles_controller

--- app/controllers/text_styles_controller.rb
+++ app/controllers/text_styles_controller.rb
@@ -1,13 +1,12 @@
 class TextStylesController < ApplicationController
   def new
     @text_style = TextStyle.new
-    @survey = Survey.find(params[:survey_id])
-    @question = Question.new
+    @survey = load_survey_from_url
   end
 
   def create
     text_style = TextStyle.new(text_style_params)
-    survey = Survey.find(params[:survey_id])
+    survey = load_survey_from_url
     if text_style.save
       survey.questions.create(style: text_style)
       redirect_to survey_path(survey)
@@ -20,5 +19,9 @@ class TextStylesController < ApplicationController
 
   def text_style_params
     params.require(:text_style).permit(:body)
+  end
+
+  def load_survey_from_url
+    Survey.find(params[:survey_id])
   end
 end
