65179bbb1fb240e698ad2d0473d5eac5ecc3f330
mattkiernan <matt.kiernan@gmail.com>
2014-11-13 10:24:54 -0500

    format update to text_answer controller

--- app/controllers/text_answers_controller.rb
+++ app/controllers/text_answers_controller.rb
@@ -3,8 +3,11 @@ class TextAnswersController < ApplicationController
     text_answer = TextAnswer.new(text_answer_params)
     question_id = params[:question_id]
     if text_answer.save
-      current_user.responses.create(answer: text_answer,
-                                    question_id: question_id)
+      current_user.responses.create(
+        answer: text_answer,
+        question_id: question_id
+      )
+
       redirect_to root_path
     end
   end
